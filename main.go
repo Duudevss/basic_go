aab76987393478bb26b07cd2a7102568a3081d0e
Duudevss <duudevss1223@gmail.com>
2026-04-07 00:32:10 +0700

    Workshop_1

--- main.go
+++ main.go
@@ -3,12 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	msg := "Patinya pimpo" + " " + "Kook!"
-	age := 28
-	price, check := 100.00, true
+	{
+		msg := "Patinya pimpo" + " " + "Kook!"
+		age := 28
+		price, check := 100.00, true
 
-	fmt.Println("Name:", msg)
-	fmt.Println("Age:", age)
-	fmt.Println("Price:", price)
-	fmt.Println("Check:", check)
+		fmt.Println("Name:", msg)
+		fmt.Println("Age:", age)
+		fmt.Println("Price:", price)
+		fmt.Println("Check:", check)
+	}
+}
+
+func workshop() {
+	{
+		movieName := "Avengers: Endgame"
+		year := 2019
+		rating := 8.4
+		genre := "Sci-Fi"
+		isSuperHero := true
+
+		fmt.Println("เรื่อง:", movieName)
+		fmt.Println("ปี:", year)
+		fmt.Println("เรตติ้ง:", rating)
+		fmt.Println("ประเภท:", genre)
+		fmt.Println("ซุปเปอร์ฮีโร่:", isSuperHero)
+	}
 }
