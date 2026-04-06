c7eea2327591cba7570a864412cfcef4e805fb65
Duudevss <duudevss1223@gmail.com>
2026-04-06 23:54:00 +0700

    Assignment Variable

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	msg := "Patinya pimpo" + " " + "Kook!"
+	age := 28
+	price, check := 100.00, true
+
+	fmt.Println("Name:", msg)
+	fmt.Println("Age:", age)
+	fmt.Println("Price:", price)
+	fmt.Println("Check:", check)
 }
